f9c2a662d72d953ed63ff3c803c1d8aa964b0632
Harri Virtanen <harri@niodi.fi>
2022-12-17 00:23:54 +0200

    Add HTTPS support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import fs from 'node:fs'
+
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
+    },
+  },
 })
